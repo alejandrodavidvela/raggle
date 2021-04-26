062bd736e1c706f9febd3cabac314f3bc51e512c
Alex Vela <avela@bpa.bumperactive.com>
2021-04-26 16:45:40 -0500

    images working

--- src/components/RaggleItems.js
+++ src/components/RaggleItems.js
@@ -1,36 +1,36 @@
 import React from 'react'
 import {Button, Card} from 'react-bootstrap'
-import uline_blanket from '../images/uline_blanket.jpg'
+import image1 from '../images/uline_blanket.jpg'
+
 
 
 
 
 const data = {
     "ulineProducts" : [
-        {"name":"Uline Blanket", "description": "warm blanket"},
-        {"name": "Uline Scarf", "description": "warm scarf"},
-        {"name": "Uline Socks", "description": "warm socks"}
+        {"name":"Uline Blanket", "description": "warm blanket", "img": image1},
+        {"name": "Uline Scarf", "description": "warm scarf", "img": image1},
+        {"name": "Uline Socks", "description": "warm socks", "img": image1}
     ]
 }
 
 const RaggleItems = () => {
   return (
-      <div>
+      <div className="ragglePrizes">
           <h2 className="raggleTitle">Prizes</h2>
           <div className="card-deck">
           {data.ulineProducts.map((item, i) => {
               return(
-                <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+                <Card key={i} style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={item.img} />
+                <Card.Body>
+                    <Card.Title >{item.name}</Card.Title>
+                    <Card.Text >
+                    {item.description}
+                    </Card.Text >
+                    <Button variant="primary">Enter</Button>
+                </Card.Body>
+                </Card>
               )
             
           })}
